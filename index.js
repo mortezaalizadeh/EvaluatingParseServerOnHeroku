be996b30b79cc36251c045ccd47c6dcb5f9ca3b8
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-03-24 11:58:30 +1300

    Adding parse server - round 1

--- index.js
+++ index.js
@@ -8,18 +8,55 @@ const SERVER_PORT = process.env.PORT || 8080;
 const SERVER_HOST = process.env.HOST || 'localhost';
 const APP_ID = process.env.APP_ID || 'my-test-app';
 const MASTER_KEY = process.env.MASTER_KEY || '70c6093dba5a7e55968a1c7ad3dd3e5a74ef5cac';
-const DATABASE_URI = process.env.DATABASE_URI || 'mongodb://localhost:27017/dev';
 const IS_DEVELOPMENT = process.env.NODE_ENV !== 'production';
 const DASHBOARD_AUTH = process.env.DASHBOARD_AUTH;
+const DATABASE_URI = process.env.DATABASE_URI || 'mongodb://localhost:27017/dev';
 
-const app = express();
+const server = express();
 
-app.set('port', SERVER_PORT);
+server.set('port', SERVER_PORT);
 
-app.get('/', function(request, response) {
+server.use(
+    '/parse',
+    new ParseServer({
+        databaseURI: DATABASE_URI,
+        appId: APP_ID,
+        masterKey: MASTER_KEY,
+        fileKey: 'f33fc1a9-9ba9-4589-95ca-9976c0d52cd5',
+        serverURL: `http://${SERVER_HOST}:${SERVER_PORT}/parse`,
+    })
+);
+
+
+let users;
+if (DASHBOARD_AUTH) {
+    const [user, pass] = DASHBOARD_AUTH.split(':');
+    users = [{
+        user,
+        pass
+    }];
+}
+
+server.use(
+    '/dashboard',
+    ParseDashboard({
+        apps: [{
+            serverURL: '/parse',
+            appId: APP_ID,
+            masterKey: MASTER_KEY,
+            appName: 'my-test-app',
+        }],
+        users,
+    }, IS_DEVELOPMENT),
+);
+
+
+console.log(users || "No username password provided to access database!!!");
+
+server.get('/', function(request, response) {
     response.send('I am still there, running and alive and now in ES6!!!');
 });
 
-app.listen(app.get('port'), function() {
-    console.log('My Node app is running on port', app.get('port'));
+server.listen(server.get('port'), function() {
+    console.log('My Node app is running on port', server.get('port'));
 });
