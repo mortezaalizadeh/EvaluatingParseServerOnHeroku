0c5a09a7bd9df679c989d35c72df271fa5820d36
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-03-24 11:35:43 +1300

    Added parse server dependency

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 import express from 'express';
+import {
+    ParseServer,
+} from 'parse-server';
+import ParseDashboard from 'parse-dashboard';
+
+const SERVER_PORT = process.env.PORT || 8080;
+const SERVER_HOST = process.env.HOST || 'localhost';
+const APP_ID = process.env.APP_ID || 'my-test-app';
+const MASTER_KEY = process.env.MASTER_KEY || '70c6093dba5a7e55968a1c7ad3dd3e5a74ef5cac';
+const DATABASE_URI = process.env.DATABASE_URI || 'mongodb://localhost:27017/dev';
+const IS_DEVELOPMENT = process.env.NODE_ENV !== 'production';
+const DASHBOARD_AUTH = process.env.DASHBOARD_AUTH;
 
 const app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', SERVER_PORT);
 
 app.get('/', function(request, response) {
-  response.send('I am still there, running and alive and now in ES6!!!');
+    response.send('I am still there, running and alive and now in ES6!!!');
 });
 
 app.listen(app.get('port'), function() {
-  console.log('My Node app is running on port', app.get('port'));
+    console.log('My Node app is running on port', app.get('port'));
 });
