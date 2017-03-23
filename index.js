5c1dca559977076e44e60cd856fa0a433c2e7571
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-03-24 11:12:18 +1300

    Changing the default message

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
-  response.send('I am there, running and alive!!!');
+  response.send('I am still there, running and alive!!!');
 });
 
 app.listen(app.get('port'), function() {
