ff84c0d3f93d87d90541c857267bc334b968cb32
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-03-24 11:23:29 +1300

    Added es6 support

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
-  response.send('I am still there, running and alive!!!');
+  response.send('I am still there, running and alive and now in ES6!!!');
 });
 
 app.listen(app.get('port'), function() {
